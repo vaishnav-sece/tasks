31a66fa93af1715e9eec7f6ba24dac878ec89f3f
vaishnav-sece <vaishnav.n2023lece@sece.ac.in>
2025-01-22 12:11:57 +0530

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,44 @@
+/*let age=80;
+
+ if(age>60){
+    console.log("you are the senior citizen");
+}
+else if(age>=18){
+    console.log("you are eligble for vote");
+}
+else{ 
+    console.log("you are not eligble");
+
+}*/
+
+
+/* let a="a";
+switch(a.toLowerCase()){
+    case "a":
+        case "b":
+            case "i":
+                case "o":
+                    case "u":
+                        console.log("vowel");
+                        break;
+                        default:
+                        console.log("constant");
+                        break;
+} */
+
+/* let a=10;
+if(a%2==0){
+    console.log("even");
+}
+else {
+    console.log("false");
+} */
+
+/*let a= -10;
+if(a>=0){
+    console.log("positive");
+}
+else {
+    console.log("negative");
+}*/
+
---
+++ day5eve.js
@@ -0,0 +1,51 @@
+
+/*for(i=0;i<50;i++){
+    console.log(i);
+}*/
+
+/*let a=50;
+let sum=0;
+for(let i=1;i<a;i++){
+   sum=sum+i;
+
+}
+console.log(sum);*/
+
+
+
+/* for(i=50;i>=0;i--){
+    console.log(i);
+} */
+
+     /*let a=5;
+    for(let i=1;i<=10;i++){
+         let s=i*a;
+         console.log(i, "*",5 "=",s);
+    } 
+
+         */
+        
+ /*    
+ const pattern = (n) =>{
+    for(i=1;i<=n;i++)
+        console.log("*".repeat(i));
+
+ }
+ pattern(7); */
+    
+
+           /*  function abc() {
+                console.log("hello world");
+
+             } */
+        /* function abc(x,y){
+    console.log(x+y);
+};
+abc(5,6); */
+
+
+          /*  let abc = (x,y) => {
+                console.log("Dear" ,x, "welcome to the company", y);
+            };
+            abc("vikasss","inforsis");
+*/
